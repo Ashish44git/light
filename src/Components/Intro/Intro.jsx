b852645c52bcc98e2e26e0dd14e7af0573e09d08
Ashish Siddhartha <ashishsiddhartha84@gmail.com>
2024-07-15 21:30:38 +0530

    first commit

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./intro.css";
+import introImage from "../../assets/introImage.png";
+const Intro = () => {
+  return (
+    <>
+      <section className="intro">
+        <div className="introContent">
+          <img src={introImage} alt="Image-Ashish" className="top " />
+          <div className="bottom">
+            Hello ! <br />
+            <span className="name animation"> I'm Ashish Siddhartha</span>{" "}
+            <br />
+            <span className="role"> Website Developer </span> <br />
+            <span className="para">
+              I am a skilled and passionate web developer with experience in
+              creating visually appealing and user-friendly websites.
+            </span>
+            <br />
+            <a href="https://drive.google.com/file/d/1m4EVq2X_14Rju4wCi2yI1lPXcFl1x4zw/view?usp=drivesdk">
+              {" "}
+              <button className="hire"> Hire Me</button>{" "}
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Intro;
